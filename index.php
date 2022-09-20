e75fe25cbec44c04cd348e601afb57f34d3f0c68
Damien Pereira <94973526+hellodamien@users.noreply.github.com>
2022-09-20 14:01:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
+<?php
+date_default_timezone_set('Europe/Paris');
+?>
 <html>
  <head>
   <title>PHP Test</title>
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
+  <?php echo '<p>Nous sommes le ' . date('d/m/Y') . ' et il est ' . date('H:i') . '</p>'; ?> 
  </body>
 </html>
